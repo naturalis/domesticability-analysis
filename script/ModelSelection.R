607bda3965e8d6006836726e3bcf7dbf935d460c
Zoe Bernard <31932066+ZoeBernard@users.noreply.github.com>
2020-07-17 12:17:49 +0200

    Removing HabitatBreadth and DietBreadth from the predictors, removing formulas and adding finalFormula for phyloglm

--- script/ModelSelection.R
+++ script/ModelSelection.R
@@ -3,9 +3,13 @@ library(phylolm)
 
 
 ## PROCESS DATASET
-# A dataset without any missing values is made. It contains the Domestication
-# trait, and also the CanonicalNames to match the data with the tree. The tree
-# tips that aren't in the final data, are dropped.
+# A dataset without any missing values is made. The X12.1_HabitatBreadth and
+# X6.1_DietBreadth are removed, due to there being not enough data for the
+# domesticated species. It contains the Domestication trait, and also the
+# CanonicalNames to match the data with the tree. The tree tips that aren't in
+# the final data, are dropped.
+predictors$X6.1_DietBreadth <- NULL
+predictors$X12.1_HabitatBreadth <- NULL
 
 modelData <- cbind(dataset$CanonicalName, dataset$Domestication,predictors)
 names(modelData)[names(modelData)=="dataset$Domestication"] <- "Domestication"
@@ -16,7 +20,6 @@ names(modelData)[names(modelData)=="dataset$CanonicalName"] <- "CanonicalName"
 
 # Only the rows without any missing values are selected
 modelData <- modelData[complete.cases(modelData),]
-#test <- modelData[rowSums(is.na(modelData)) == 1,]
 
 # Dropping species from the tree
 dropTips <- setdiff(tree$tip.label, modelData$CanonicalName)
@@ -35,65 +38,23 @@ modelData$Domestication[modelData$Domestication==2] <- 0
 # The input formulas are defined, which are going to be used as input for the
 # phyloglmstep function.
 
-# Formula1: All predictors
-# Results: Current model: Domestication ~ 1 + X15.1_LitterSize + Horns_Antlers + NaturalPredators + AVGMovingSpeed - AIC(k=2): 19.2464091190541
-formula1 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula2: Without X5.1_AdultBodyMass_g
-# Results: 
-formula2 <- Domestication ~ X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula3: Without X1.1_ActivityCycle
-# Results: 
-formula3 <- Domestication ~ X5.1_AdultBodyMass_g + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula4: Without X6.1_DietBreadth
-# Results: 
-formula4 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula5: Without X12.1_HabitatBreadth
-# Results: 
-formula5 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula6: Without X15.1_LitterSize
-# Results: 
-formula6 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula7: Without X21.1_PopulationDensity_n_km2
-# Results: 
-formula7 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula8: Without X10.2_SocialGrpSize
-# Results: 
-formula8 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula9: Without Sociality
-# Results: 
-formula9 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula10: Without SocialHierarchy
-# Results: 
-formula10 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula11: Without MatingSystem
-# Results: 
-formula11 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula12: Without YearRoundBreeding
-# Results: 
-formula12 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
-
-# Formula13: Without DevelopmentStrategy
-# Results: 
-formula13 <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X6.1_DietBreadth + X12.1_HabitatBreadth + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
+# Formula with all the predictors
+# Results:  Current model: 
+#           AIC(k=2): 28.3458557864183
+formula <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
 
 
-### MODEL SELECTION
+## MODEL SELECTION
 # Using the phyloglmstep.
-phyloglmstep(formula1, starting.formula = NULL, data=modelData, phy=modelTree, 
+phyloglmstep(formula, starting.formula = NULL, data=modelData, phy=modelTree, 
              method="logistic_MPLE", direction = "forward", trace = 2, 
              btol = 30, log.alpha.bound = 4, start.beta=NULL, 
              start.alpha=NULL, boot = 0, full.matrix = TRUE, k=2)
 
+## FINAL MODEL
+# The final formula for the model constructed by the phyloglmstep function
+finalFormula <- Domestication ~ 1 + X5.1_AdultBodyMass_g + DevelopmentStrategy + Horns_Antlers + AVGMovingSpeed + AVGTravelDistance
 
-rm(dropTips)
+
+rm(dropTips, formula, modelData, modelTree)
+
