14a87d422e6baf5545fe6101a44217ce388298b8
KesLippert <kes.lippert@naturalis.nl>
2020-07-20 17:27:06 +0200

    Removed AdultBodyMass, played around with btol and log.alpha.bound in phyloglmstep, and cleaned up text.

--- script/ModelSelection.R
+++ script/ModelSelection.R
@@ -3,11 +3,11 @@ library(phylolm)
 
 
 ## PROCESS DATASET
-# A dataset without any missing values is made. The X12.1_HabitatBreadth and
+# A dataset without missing values is made. The X12.1_HabitatBreadth and
 # X6.1_DietBreadth are removed, due to there being not enough data for the
 # domesticated species. It contains the Domestication trait, and also the
 # CanonicalNames to match the data with the tree. The tree tips that aren't in
-# the final data, are dropped.
+# the final data are dropped.
 predictors$X6.1_DietBreadth <- NULL
 predictors$X12.1_HabitatBreadth <- NULL
 
@@ -41,14 +41,14 @@ modelData$Domestication[modelData$Domestication==2] <- 0
 # Formula with all the predictors
 # Results:  Current model: 
 #           AIC(k=2): 28.3458557864183
-formula <- Domestication ~ X5.1_AdultBodyMass_g + X1.1_ActivityCycle + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
+formula <- Domestication ~ X1.1_ActivityCycle + X15.1_LitterSize + X21.1_PopulationDensity_n_km2 + X10.2_SocialGrpSize + Sociality + SocialHierarchy + MatingSystem + YearRoundBreeding + DevelopmentStrategy + Horns_Antlers + Lifespan + NaturalPredators + AVGMovingSpeed + AVGTravelDistance + Aspect + ClayPercentage + PETWettestQuarter + OrganicCarbon
 
 
 ## MODEL SELECTION
 # Using the phyloglmstep.
 phyloglmstep(formula, starting.formula = NULL, data=modelData, phy=modelTree, 
              method="logistic_MPLE", direction = "forward", trace = 2, 
-             btol = 30, log.alpha.bound = 4, start.beta=NULL, 
+             btol = 40, log.alpha.bound = 4, start.beta=NULL, 
              start.alpha=NULL, boot = 0, full.matrix = TRUE, k=2)
 
 ## FINAL MODEL
@@ -57,4 +57,5 @@ finalFormula <- Domestication ~ 1 + X5.1_AdultBodyMass_g + DevelopmentStrategy +
 
 
 rm(dropTips, formula, modelData, modelTree)
+
 
