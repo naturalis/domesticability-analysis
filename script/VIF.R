e7f1ee5be8beaa3591555b272f89799c784950c3
Zoe Bernard <31932066+ZoeBernard@users.noreply.github.com>
2020-08-06 16:37:48 +0200

    Cleaning up script

--- script/VIF.R
+++ script/VIF.R
@@ -1,13 +1,16 @@
-# IMPORT PACKAGES
+## IMPORT PACKAGES
 library(usdm)
 library(caret)
 library(corrplot)
+
 
 ## BEGIN VALUES
 cat("The first correlation matrix is a visualization of the dataset without the removal of any traits. There is a lot of collinearity present. The VIF analysis will take care of this.")
 startPredictors <- dataset[,c(8:78)]
 matrix <- cor(startPredictors, use = "pairwise.complete.obs")
 corrplot(matrix, type="lower", order = "hclust", tl.pos = "l", tl.col = "black", tl.cex = 0.5)
+
+
 ## CHECK CORRELATION MATRIX
 # The corrplot package is used to visualize the collinearity between the traits,
 # using the correlation matrix. With the findCorrelation() function, the columns
@@ -22,6 +25,8 @@ for(cut in cutoffValues){
   startPredictors <- startPredictors[,-c(delColumn)]
 }
 vif(startPredictors)
+
+
 ## INSERT ADULTBODYMASS
 # The set of predictors is declared for the VIF analysis, and a last check for
 # collinearity using the visualized matrix is done. During the first round the
@@ -29,16 +34,21 @@ vif(startPredictors)
 # Adultbodymass is hypothesized to be a biologically relevant trait, it will be reinserted in this step.
 vifPredictors <- cbind(dataset$X5.1_AdultBodyMass_g, startPredictors)
 colnames(vifPredictors)[1] <- "X5.1_AdultBodyMass_g"
+
 # Check VIF -> Adultbodymass is highly correlated with the other traits.
 vif(vifPredictors)
+
 # Check correlation matrix
 matrix <- cor(vifPredictors, use = "pairwise.complete.obs")
 cat("The second corrplot is a visualization of the dataset after cutoff values are implemented and adultbodymass is added.")
 corrplot(matrix, type="lower", order = "hclust", tl.pos = "l", tl.col = "black", tl.cex = 0.7)
+
 # Remove highest collinearity trait after adult body mass to see if it brings the value below 10
 vifPredictors$AVGFoodConsumption <- NULL
+
 # Check VIF -> normal values (<10), we are good to go
 vif(vifPredictors)
+
 
 ## FINAL PREDICTOR SET
 # Final set of predictors, containing:  X5.1_AdultBodyMass_g, X1.1_ActivityCycle,
@@ -49,9 +59,11 @@ vif(vifPredictors)
 # Aspect, ClayPercentage, PETWettestQuarter, OrganicCarbon, bio18
 
 predictors <- vifPredictors
+
 ## FINAL VALUES
 cat("The third correlation matrix is our final product after removing all highly correlated traits.")
 matrix <- cor(predictors, use = "pairwise.complete.obs")
 corrplot(matrix, type="lower", order = "hclust", tl.pos = "l", tl.col = "black", tl.cex = 0.5)
+
 ## REMOVE VARIABLES
 rm(vifPredictors, startPredictors, matrix, cut, cutoffValues, delColumn)
